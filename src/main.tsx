f6aad745383cbf740b9ff418e30cbcdbbcb7e0b3
mackcamargo <mackcamargo@gmail.com>
2026-07-31 14:25:42 -0300

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 
+// Google Analytics
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-MP6SWJVQGV";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-MP6SWJVQGV');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
