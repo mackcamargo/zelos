0c213b0cbc9835ffac1a7aa02ef54f5cb912f314
mackcamargo <mackcamargo@gmail.com>
2026-07-31 14:46:45 -0300

    Correct rendering of StrictMode and ThemeProvider
    Fixed rendering of StrictMode and ThemeProvider components.

--- src/main.tsx
+++ src/main.tsx
@@ -5,25 +5,9 @@ import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
-        <StrictMode>
-            <ThemeProvider>
-                  <App />
-            </ThemeProvider>ThemeProvider>
-        </StrictMode>StrictMode>,
-      );
-
-// Google Analytics - Load after React renders
-setTimeout(() => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MP6SWJVQGV';
-        document.head.appendChild(script);
-      
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-                  window.dataLayer.push(arguments);
-        }
-        (window as any).gtag = gtag;
-        gtag('js', new Date());
-        gtag('config', 'G-MP6SWJVQGV');
-}, 0);</StrictMode>
+          <StrictMode>
+              <ThemeProvider>
+                    <App />
+              </ThemeProvider>ThemeProvider>
+          </StrictMode>StrictMode>,
+        );
